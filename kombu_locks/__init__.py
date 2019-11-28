58f43857f28711770c0d96edbe249fa27556be1f
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2019-11-28 01:26:01 -0300

    functional?? version of a lock

--- kombu_locks/__init__.py
+++ kombu_locks/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """Karel Antonio Verdecia Ortiz"""
 __email__ = 'kverdecia@gmail.com'
 __version__ = '0.1.0'
+
+
+from .kombu_locks import Lock, LockError
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['kombu']
 
 setup_requirements = [ ]
 
@@ -25,6 +25,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
